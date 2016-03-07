37a51f60561ce38e191e4fd688dde6f4153d5324
ericbgauch <ericbgauch@hotmail.com>
2016-03-07 19:17:42 -0300

    Update EX4_Botao_LED.ino

--- EX4_Botao_LED/EX4_Botao_LED.ino
+++ EX4_Botao_LED/EX4_Botao_LED.ino
@@ -1,17 +1,15 @@
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
-int buttonState = 0;         // variable for reading the pushbutton status
+int botao = 0;  //Cria uma variável que irá guardar o valor do botao
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
+  pinMode(13, OUTPUT);   //Inicializa o led conectado no pino 13
+  pinMode(2, INPUT);     //Inicializa o botao conectado no pino 2
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    digitalWrite(ledPin, HIGH);
-  } else {
-    digitalWrite(ledPin, LOW);
+  botao = digitalRead(2);   //Pergunta ao botao se ele está sendo apertado ou não
+  if (botao == HIGH) {      //Se sim, o valor dele será "HIGH"
+    digitalWrite(13, HIGH); //Liga o led conectado no pino 13
+  } else {                  //Se o botão não está sendo apertado
+    digitalWrite(13, LOW);  //Desliga o led
   }
 }
