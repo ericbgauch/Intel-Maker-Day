cdf4000cd4379b89285375e7e993ca9f8a08d3cd
ericbgauch <ericbgauch@hotmail.com>
2016-03-07 19:31:15 -0300

    Update EX6_Proximidade_LED.ino

--- EX6_Proximidade_LED/EX6_Proximidade_LED.ino
+++ EX6_Proximidade_LED/EX6_Proximidade_LED.ino
@@ -1,25 +1,25 @@
-#define echoPin 7 // Echo Pino
-#define trigPin 8 // Trigger Pino
-#define LEDPin 9
+#define pinoEco 7  //Define o pino 7 com o nome "pinoEco"
+#define pinoTrigger 8 //Define o pino 8 com o nome "pinoTrigger"
+#define LEDPin 13  //Define o pino 13 com o nome "LEDPin"
 
-int maximumRange = 200; // máximo valor
-int minimumRange = 10; // mínimo valor
-long duration, distance; // duração e distância
+int maximumRange = 200;   // máximo valor
+int minimumRange = 10;    // mínimo valor
+long duration, distance;  // duração e distância
 
 void setup() {
  Serial.begin (9600);
- pinMode(trigPin, OUTPUT);
- pinMode(echoPin, INPUT);
+ pinMode(pinoTrigger, OUTPUT);
+ pinMode(pinoEco, INPUT);
  pinMode(LEDPin, OUTPUT);
 }
 
 void loop() {
- digitalWrite(trigPin, LOW);
+ digitalWrite(pinoTrigger, LOW);
  delayMicroseconds(2);
- digitalWrite(trigPin, HIGH);
+ digitalWrite(pinoTrigger, HIGH);
  delayMicroseconds(10);
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH);
+ digitalWrite(pinoTrigger, LOW);
+ duration = pulseIn(pinoEco, HIGH);
  distance = duration/58.2;
 
  if (distance >= maximumRange || distance <= minimumRange){
